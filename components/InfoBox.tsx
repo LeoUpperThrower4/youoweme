b91dbd96bbe9979e962dbdcfaca4094e38dbb526
Leonardo dos Santos Duarte Silva <ldsds94@gmail.com>
2022-08-30 20:22:29 -0300

    created component to show information on screen

---
+++ components/InfoBox.tsx
@@ -0,0 +1,16 @@
+type InfoBoxProps = {
+  title: string,
+  children: React.ReactNode,
+  className?: string,
+}
+
+export default function InfoBox({ title, children, className }: InfoBoxProps) {
+  return ( 
+    <div className={`w-screen p-4 border-2 rounded max-w-md ${className}`}>
+      <div className='flex justify-center'>
+        <h1 className='font-bold text-lg'>{ title }</h1>
+      </div>
+      {children}
+  </div>
+  )
+}
