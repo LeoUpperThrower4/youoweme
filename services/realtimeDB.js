82dbdff7241a2d0743c6cd264ae571065c331524
Leonardo dos Santos Duarte Silva <ldsds94@gmail.com>
2022-09-21 20:01:37 -0300

    removed unused files

--- services/realtimeDB.js
+++
@@ -1,7 +0,0 @@
-export function writeData(path, data) {
-  localStorage.setItem(path, JSON.stringify(data));
-}
-
-export function readData(path) {
-  return JSON.parse(localStorage.getItem(path));
-}
