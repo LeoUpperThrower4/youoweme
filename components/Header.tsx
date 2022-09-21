fc9a8f26fc80311840a829c0c8057b4ab70d9e1a
Leonardo dos Santos Duarte Silva <ldsds94@gmail.com>
2022-09-21 20:03:23 -0300

    moved modal from header to index

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,172 +1,18 @@
-import Modal from 'react-modal';
-import { useState } from 'react';
-import useGroups from '../hooks/useGroups';
-import { writeData } from '../services/realtimeDB';
-import { useRouter } from 'next/router';
-import { useAuth } from '../hooks/useAuth';
-
-Modal.setAppElement('#__next');
-
-const customModalStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-  },
-};
-
 type HeaderProps = {
   update: Boolean
+  children: React.ReactNode
 }
 
-export default function Header({ update }: HeaderProps) {
-  const { addGroup, groups, updateGroups } = useGroups();
-  const { username } = useAuth();
-  const router = useRouter();
-
-  // Estados da modal de criação de grupo
-  const [ groupModalIsOpen, setGroupModalIsOpen ] = useState(false);
-  const [ groupName, setGroupName ] = useState('');
-
-  // Estados da modal de criação de transação
-  const [ transactionModalIsOpen, setTransactionModalIsOpen ] = useState(false);
-  const [ transactionFrom, setTransactionFrom ] = useState('');
-  const [ transactionTo, setTransactionTo ] = useState('');
-  const [ transactionValue, setTransactionValue ] = useState(0);
-  const [ transactionDescription, setTransactionDescription ] = useState('');
-
-  function handleCreateGroupClick() {
-    openGroupModal()
-  }
-  function openGroupModal() {
-    setGroupModalIsOpen(true);
-    setTransactionModalIsOpen(false);
-  }
-  function openTransactionModal() {
-    setGroupModalIsOpen(false);
-    setTransactionModalIsOpen(true);
-  }
-  function handleCloseModal() {
-    if (groupModalIsOpen) {
-      setGroupModalIsOpen(false);
-      setGroupName('');
-    }
-    if (transactionModalIsOpen) {
-      setTransactionModalIsOpen(false);
-      setTransactionFrom('');
-      setTransactionTo('');
-      setTransactionValue(0);
-      setTransactionDescription('');
-    }
-  }
-
-  function createGroup(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-    event.preventDefault();
-    if (!update) {
-      if (addGroup({
-        name: groupName,
-        participants: [username || ''],
-        transactions: [], // { from: '', to: '', value: 0, description: 'initial' , datetime: new Date().toISOString() }
-        allGroupDebts: [],
-      })) {
-        // toast.success('Group created successfully!', {
-        //   position: toast.POSITION.TOP_RIGHT,
-        // });
-      } else {
-        // toast.error('Group with this name already exists!', {
-          //   position: toast.POSITION.TOP_RIGHT,
-          // });
-        }
-      router.push(`/${groupName}`);
-    } else {
-      const groupId = groups.findIndex(group => group.name === router.query.groupName);
-      if (groupId !== -1) {
-        const newGroup = groups[groupId];
-        newGroup.name = groupName;
-        groups.splice(groupId, 1, newGroup);
-        writeData('groups', groups);
-        router.push(`/${groupName}`);
-      // toast.success('Group updated successfully!', {
-      //   position: toast.POSITION.TOP_RIGHT,
-      // });
-      }
-    }
-    handleCloseModal();
-  }
-
-  function createTransaction() {
-    const groupId = groups.findIndex(group => group.name === router.query.groupName);
-    if (groupId !== -1) {
-      const transaction = {
-        from: transactionFrom,
-        to: transactionTo,
-        value: transactionValue,
-        description: transactionDescription,
-        datetime: new Date().toISOString(),
-      };
-      groups[groupId].transactions.push(transaction);
-      // Deve haver uma validação aqui
-      writeData('groups', groups)
-      updateGroups()
-    }
-    // toast.success('Transaction created successfully!', {
-    //   position: toast.POSITION.TOP_RIGHT,
-    // });
-    handleCloseModal();
-  }
-
+export default function Header({ children }: HeaderProps): JSX.Element {
   return (
     <>
       <header className="bg-cyan-600">
         <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
           <div className="flex justify-center">
-            <button onClick={handleCreateGroupClick} className="rounded border-2 border-cyan-700 px-4 py-2 my-4 text-slate-100">{update? 'Atualizar': 'Criar'} Grupo</button>
-            { router.query.groupName &&
-            <button onClick={() => openTransactionModal()} className='rounded border-2 border-cyan-700 px-4 py-2 my-4 ml-2 bg-amber-200 hover:bg-amber-400'>Adicionar transação</button>}
+            {children}
           </div>
         </nav>
       </header>
-      <Modal
-        isOpen={groupModalIsOpen || transactionModalIsOpen}
-        onRequestClose={handleCloseModal}
-        contentLabel={transactionModalIsOpen? "Adicionar Transação": "Criar Grupo"}
-        style={customModalStyles}
-      >
-        {transactionModalIsOpen?(
-          <div>
-          <div className='flex justify-between border-b mb-1 py-2'>
-            <h1>Adicione uma transação!</h1>
-            <button onClick={handleCloseModal} className='ml-4'>X</button>
-          </div>
-          <form className='flex flex-col mt-2'>
-            <input type="text" placeholder='De' className='border p-2 rounded' onChange={(e) => { setTransactionFrom(e.target.value) }} /> 
-            <input type="text" placeholder='Para' className='border p-2 rounded' onChange={(e) => { setTransactionTo(e.target.value) }} />
-            <input type="text" placeholder='Valor (ex: 15.50)' className='border p-2 rounded' onChange={(e) => { setTransactionValue(Number(e.target.value)) }} />
-            <input type="text" placeholder='Descrição' className='border p-2 rounded' onChange={(e) => { setTransactionDescription(e.target.value) }} />
-            <button className='border p-2 rounded mt-2 bg-cyan-600 text-white hover:bg-cyan-700' onClick={() => createTransaction()}>
-              Criar
-            </button>
-          </form>
-        </div>
-        ):(
-          <div>
-            <div className='flex justify-between border-b mb-1 py-2'>
-              <h1>{update? 'Atualize': 'Crie'} seu grupo!</h1>
-              <button onClick={handleCloseModal} className='ml-4'>X</button>
-            </div>
-            <form className='flex flex-col mt-2'>
-              <input type="text" placeholder='Nome do grupo' className='border p-2 rounded' onChange={(e) => { setGroupName(e.target.value) }} />
-              <button onClick={(event) => createGroup(event)} className='border p-2 rounded mt-2 bg-cyan-600 text-white hover:bg-cyan-700'>
-              {update? 'Atualizar': 'Criar'}
-              </button>
-            </form>
-          </div>
-        )}
-
-      </Modal>
     </>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,80 @@
+import Modal from 'react-modal';
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
 import Header from '../components/Header'
 import useGroups from '../hooks/useGroups'
+import { useRef, useState } from 'react';
+import { Group } from '../interfaces/Groups';
+
+Modal.setAppElement('#__next');
+
+const customModalStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
 
 const Home: NextPage = () => {
-  const { groups } = useGroups();
+  const { groupsSummary, addGroup } = useGroups();
   const router = useRouter()
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+  const groupNameInput = useRef(null) // HTMLInputElement. não consegui tipar corretamente
 
-  function handleGroupBoxClick(group: any) {
+  function handleGroupBoxClick(group: Group) {
     router.push(group.name)
+  }
+
+  function handleCreateGroupClick(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    event.preventDefault()
+    const groupName = groupNameInput.current.value
+    if (groupName) {
+      if (addGroup(groupName)) {
+      } else {
+        alert('Ocorreu um erro ao criar o grupo')
+      }
+    }
+    setModalIsOpen(false)
   }
 
   return (
     <>
-      <Header />
+      <Header>
+        <button onClick={() => {setModalIsOpen(true)}} className="rounded border-2 border-cyan-700 px-4 py-2 my-4 text-slate-100">Criar Grupo</button>
+      </Header>
       <div className='flex justify-center'>
         <div className='w-full max-w-md mt-2'>
-          {groups && groups.map((group: any) => (
-              <div onClick={(e) => { handleGroupBoxClick(group) }} className='px-4 py-6 flex justify-center border border-black rounded cursor-pointer mb-1 hover:bg-slate-300' key={group.name}>
+          {groupsSummary && groupsSummary.map((group: Group) => (
+              <div onClick={() => { handleGroupBoxClick(group) }} className='px-4 py-6 flex justify-center border border-black rounded cursor-pointer mb-1 hover:bg-slate-300' key={group.name}>
                 <h1>{group.name}</h1>
               </div>
           ))}
         </div>
       </div>
+      <Modal
+        isOpen={modalIsOpen}
+        contentLabel="Criar Grupo"
+        style={customModalStyles}
+      >
+        
+        <div>
+          <div className='flex justify-between border-b mb-1 py-2'>
+            <h1>Crie seu grupo!</h1>
+            <button onClick={() => {setModalIsOpen(false)}} className='ml-4'>X</button>
+          </div>
+          <form className='flex flex-col mt-2'>
+            <input ref={groupNameInput} type="text" placeholder='Nome do grupo' className='border p-2 rounded' />
+            <button onClick={(e) => {handleCreateGroupClick(e)}} className='border p-2 rounded mt-2 bg-cyan-600 text-white hover:bg-cyan-700'>
+            Criar
+            </button>
+          </form>
+        </div>
+      </Modal>
     </>
 )}
 
 export default Home;
-
